7fc9686f48bef5eff88cbdaee8bfb972675e38a9
Anjali <anjali.payal69@gmail.com>
2020-04-21 19:30:48 +0200

    Chapter 5

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import Products from './Products';
 import { Button } from 'react-bootstrap';
 import Rating from './Rating';
+import JumboTronComponent from './JumboTronComponent';
 
 class App extends Component {
   render() {
-    const isValid = true;
     return (
-    <div>
-      <Products />
-    </div>
+      <div>
+        <JumboTronComponent>
+          This is a long sentence, and I want to insert content into the
+          jumbotron component from the outside.
+        </JumboTronComponent>
+      </div>
     );
   }
 }
